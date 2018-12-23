c1088193a55e5cf26535df23a4a29f4156fd2767
Unknown <bilroum@gmail.com>
2018-12-23 13:49:47 +0100

    Issue #6 Show all meetings for a User
    Added the View of all Meetings for a User, displayed in Cards.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -3,18 +3,24 @@ import { NavController } from 'ionic-angular';
 import { TeaCoApiProvider } from "../../providers/teaco-api-provider/teaco-api-provider";
 import {User} from "../../models/User";
 import { AddNewMeetingPage } from '../add-new-meeting/add-new-meeting';
+import { Meeting } from '../../models/Meeting';
+import { Vote } from '../../models/Vote';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  meetings: Meeting [];
+  
   constructor(private navCtrl: NavController, private apiService: TeaCoApiProvider) {
     // //TODO: just for testing purpose --> should be removed later on
-    // this.apiService.getAllMeetings('1').subscribe(meetings => {
-    //   console.log(meetings);
-    // })
+     this.apiService.getAllMeetings("1").subscribe(meetings => {
+       this.meetings = meetings;
+      console.log(this.meetings);
+     })
+
+   
   }
   
   addNewMeetingPage(){
